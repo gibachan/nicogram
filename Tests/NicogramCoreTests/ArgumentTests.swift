20644d8b3dd97552510ff26c54af183f07881c6c
gibachan <childhoodend.tk@gmail.com>
2017-07-27 08:44:48 +0900

    Add test

---
+++ Tests/NicogramCoreTests/ArgumentTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import NicogramCore
+
+class ArgumentTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    func testExtractVideId() {
+        let url = "http://www.nicovideo.jp/watch/sm8628149"
+        XCTAssertEqual(extractVideoId(url), "sm8628149")
+    }
+
+    func testParseArguments() {
+        let arguments = ["Nicogram", "http://www.nicovideo.jp/watch/sm8628149", "--email", "xxxxx@sample.com", "--password", "ABC012"]
+        let result = try! parseArguments(arguments)
+        XCTAssertEqual(result.0, "xxxxx@sample.com")
+        XCTAssertEqual(result.1, "ABC012")
+        XCTAssertEqual(result.2, "http://www.nicovideo.jp/watch/sm8628149")
+    }
+}
